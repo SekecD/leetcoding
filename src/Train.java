b39f3531645eb226cf2b261366fdae1b15833d5a
Your Name <you@example.com>
2024-02-12 18:46:09 +0300

    igra

--- src/Train.java
+++ src/Train.java
@@ -1,5 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Train {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(10) + 1;
+
+        System.out.println("Can you guess it number ?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts = 0;
+
+        do {
+            System.out.println("Enter your guess");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < randomNumber) {
+                System.out.println("Too low!");
+            } else if (guess > randomNumber) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Congratulations! You guessed it right!");
+                System.out.println("You took " + attempts);
+            }
+        } while (guess != randomNumber);
     }
 }

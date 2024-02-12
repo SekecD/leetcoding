7a9c6294f29dbded8c09d93d1b2f5cb24efc8277
Your Name <you@example.com>
2024-02-12 18:59:38 +0300

    igra1

--- src/Train.java
+++ src/Train.java
@@ -5,29 +5,42 @@ public class Train {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
-        Random random = new Random();
-        int randomNumber = random.nextInt(10) + 1;
-
-        System.out.println("Can you guess it number ?");
-
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int score = 0;
 
-        int guess;
-        int attempts = 0;
+        for (int i = 0; i < 10; i++) {
+            int num1, num2, result;
 
-        do {
-            System.out.println("Enter your guess");
-            guess = scanner.nextInt();
-            attempts++;
+            if (i < 5) {
+                num1 = random.nextInt(10) + 1;
+                num2 = random.nextInt(10) + 1;
+                result = num1 + num2;
 
-            if (guess < randomNumber) {
-                System.out.println("Too low!");
-            } else if (guess > randomNumber) {
-                System.out.println("Too high!");
             } else {
-                System.out.println("Congratulations! You guessed it right!");
-                System.out.println("You took " + attempts);
+
+                num1 = random.nextInt(90) + 10;
+                num2 = random.nextInt(90) + 10;
+                result = num1 * num2;
             }
-        } while (guess != randomNumber);
+
+            if (i < 5) {
+                System.out.println("Stage 1 - Question #" + (i + 1) + ":");
+                System.out.println("What is the sum of " + num1 + " and " + num2 + "?");
+            } else {
+                System.out.println("Stage 2 - Question #" + (i - 4) + ":");
+                System.out.println("What is the product of " + num1 + " and " + num2 + "?");
+            }
+            int userAnswer = scanner.nextInt();
+
+            if (userAnswer == result) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("Wrong!");
+            }
+            System.out.println();
+        }
+        System.out.println("Game over!");
     }
 }

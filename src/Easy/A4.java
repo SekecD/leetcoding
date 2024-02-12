6ca5998317ab65d5dab214f099374fa221d0db5d
Your Name <you@example.com>
2024-02-12 18:40:18 +0300

    first commit11zz11zaaaф

--- src/Easy/A4.java
+++ src/Easy/A4.java
@@ -1,2 +1,25 @@
-package Easy;public class A4 {
+package Easy;
+
+public class A4 {
+    public int majorityElement(int[] nums) {
+        int majorityElement = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == majorityElement) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    majorityElement = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return majorityElement;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/Medium/A3repair.java
+++ src/Medium/A3repair.java
@@ -1,2 +1,24 @@
-package Medium;public class A3repair {
+package Medium;
+public class A3repair {
+    public boolean winnerOfGames(String colors) {
+        int aliceCount = 0;
+        int bobCount = 0;
+        int i = 1;
+
+        while (i < colors.length() - 1) {
+            int streak = 1;
+            char color = colors.charAt(i);
+            while (i < colors.length() - 1 && colors.charAt(i) == color) {
+                streak++;
+                i++;
+            }
+            if (color == 'A' && streak >= 3) {
+                aliceCount += streak - 2;
+            } else if (color == 'B' && streak >= 3) {
+                bobCount += streak - 2;
+            }
+            i++;
+        }
+        return aliceCount > bobCount;
+    }
 }
